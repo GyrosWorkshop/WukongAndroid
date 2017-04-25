1f472d99a232ffd73087d31ca761bd520fe3eaf5
Senorsen <senorsen.zhang@gmail.com>
2017-04-25 20:39:02 +0800

    services skeleton

---
+++ app/src/main/java/com/senorsen/wukong/service/WukongService.kt
@@ -0,0 +1,16 @@
+package com.senorsen.wukong.service
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class WukongService : Service() {
+
+    override fun onBind(intent: Intent?): IBinder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
